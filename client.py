746bf6801f604004591a20f3e4449cc681aff0c6
Zhang <Yuxuan.Zhang@rabobank.nl>
2023-03-01 22:05:29 +0100

    Add all exisiting files of the project to git

---
+++ client.py
@@ -0,0 +1,13 @@
+
+class Client:
+
+# A simple class
+# attribute
+    name = "Danny"
+    age = 45
+    default = False
+
+    # A sample method
+    def fun(self):
+        print("I'm a", self.attr1)
+        print("I'm a", self.attr2)
