2872ae96feef3312638fa5201ae5a3314c4891ae
Ramanathan Varadharajan <ramanathan.varadharajan@rabobank.nl>
2023-03-07 17:05:11 +0100

    Removed a bug, no attr1, attr2 present.

--- src/client.py
+++ src/client.py
@@ -10,8 +10,8 @@ class Client:
 
     # A sample method
     def fun(self):
-        print("I'm a", self.attr1)
-        print("I'm a", self.attr2)
+        print("I'm a", self.name)
+        print("I'm a", self.age)
         return None
 
     def info(self):
