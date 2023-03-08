5af7716093d43822493c28e859f1f369cad61927
Ramanathan Varadharajan <ramanathan.varadharajan@rabobank.nl>
2023-03-08 09:38:53 +0100

    Added a print statement.

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('test')
-
+print("Hellow from pipey.")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
