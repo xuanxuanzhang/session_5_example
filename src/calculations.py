f478dc9913ee43bfb459ed59cab1b081126b3128
Ramanathan Varadharajan <ramanathan.varadharajan@rabobank.nl>
2023-03-08 16:11:11 +0100

    Added gitignore and failing tests.

---
+++ src/calculations.py
@@ -0,0 +1,5 @@
+def sum(*args):
+    c=0
+    for item in args:
+        c+= item
+    return c
--- tests/test_one.py
+++ tests/test_one.py
@@ -1,2 +1,12 @@
-def test_one():
-    pass
+from src.calculations import sum
+
+
+def test_simple_sum():
+    a = 9
+    b = 10
+    a_plus_b = sum(a, b, a)
+
+    # expected output
+    a_plus_b_exp = 29
+
+    assert a_plus_b_exp == a_plus_b
