f8a1f6b0858041705838e37edeb1188357aea281
Ramanathan Varadharajan <ramanathan.varadharajan@rabobank.nl>
2023-03-08 16:58:27 +0100

    Add failing tests.

--- tests/your_name/test_calculations.py
+++ tests/your_name/test_calculations.py
@@ -2,8 +2,10 @@ from src.calculations import sum, product
 
 
 def test_sum():
-    pass
+    a, b, c = 1, 2, 4
+    assert 8 == sum(a, b, c, a)
 
 
 def test_product():
-    pass
+    a, b, c = 1, 2, 4
+    assert 9 == product(a, b, c, a)
