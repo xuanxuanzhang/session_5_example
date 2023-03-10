c8eb189955ef865f7c30913c2e48d62666d6f4e6
cosmin.ichim <cosmin.ichim@rabobank.com>
2023-03-10 13:06:14 +0200

    the first commit on Rabobank

--- tests/your_name/test_calculations.py
+++ tests/your_name/test_calculations.py
@@ -1,6 +1,4 @@
 from src.calculations import sum, product
-
-
 def test_sum():
     a, b, c = 1, 2, 4
     assert 8 == sum(a, b, c, a)
@@ -9,3 +7,4 @@ def test_sum():
 def test_product():
     a, b, c = 1, 2, 4
     assert 9 == product(a, b, c, a)
+#comment to test commit new
