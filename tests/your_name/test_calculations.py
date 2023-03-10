71bdd69c8b5a71e3198ba5efb879cc2997f17b4b
FaconE <emilie.facon@rabobank.nl>
2023-03-10 11:48:39 +0100

    not sure

---
+++ tests/your_name/test_calculations.py
@@ -0,0 +1,11 @@
+from src.calculations import sum, product
+
+
+def test_sum():
+    a, b, c = 1, 2, 4
+    assert 8 == sum(a, b, c, a)
+
+
+def test_product():
+    a, b, c = 1, 2, 4
+    assert 8 == product(a, b, c, a)
